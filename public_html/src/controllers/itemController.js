a8c4cdc11a5559319b4b5b7a45dd76e0d119b910
bengrich <webmasterbengrich@gmail.com>
2016-04-03 20:31:46 +0100

    persistance

--- public_html/src/controllers/itemController.js
+++ public_html/src/controllers/itemController.js
@@ -7,8 +7,13 @@ app
     .controller('itemList', function ($scope, itemProvider) {
         $scope.items = itemProvider.getItems();
     })
-    .controller('itemCreate', function ($scope, categoryProvider) {
+    .controller('itemCreate', function ($scope, categoryProvider, itemProvider) {
         $scope.categories = categoryProvider.getCategories();
+        $scope.items = itemProvider.getItems();
+        
+        $scope.createItem = function (item){
+            itemProvider.create(item);
+        }
     })
     .controller('itemRemove', function ($scope) {
 
--- public_html/src/services/itemProvider.js
+++ public_html/src/services/itemProvider.js
@@ -9,6 +9,11 @@ app.service('itemProvider', function (){
     this.getItems = function (){
         return items;
     }
+    this.create = function (item){
+        items.push(item);
+        
+        return items;
+    }
 });
 
 
